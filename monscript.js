58b94de977a66b78a3c378e232bc96e9ad1c2668
Timothée Poux <timothee.poux@gmail.com>
2026-02-04 12:00:39 +0100

    TP8

--- monscript.js
+++ monscript.js
@@ -1,29 +1,7 @@
-/*
-let tabJours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-let tabMois = ["Janvier, Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
-let maDateV1 = {
-    "jour" : tabJours[2],
-    "mois" : tabMois[1]
-};
-let maDateV2 = {};
-maDateV2.jour = tabJours[2];
-maDateV2.mois = tabMois[1];
+let number = 5;
+let text = '5';
+let isRainingToday = false;
 
-let nbr1 = 3;
-let nbr2 = 7;
-let nbr3 = 8;
-let nbr4 = 21;
-
-console.log(nbr1 + nbr2, nbr4 - nbr3);
-
-nbr2 = nbr2*nbr1;
-console.log(nbr2);
-nbr2++;
-console.log(nbr2);
-
-console.log(nbr4/nbr1, nbr4%nbr1);
-*/
-
-let nomSerie = prompt("Quelle est ta série préférée ?");
-let nomPerso  = prompt("Quel est ton personnage préférée ?");
-alert("Ma série préférée est " + nomSerie + " et mon personnage est " + nomPerso + " !");
+console.log(number == text);
+console.log(number === text);
+if (isRainingToday) console.log("Where is my Umbrella ?");
