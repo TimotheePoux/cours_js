a00896c98e8f6c33c3951efe7f8f2764ef28b521
Timothée Poux <timothee.poux@gmail.com>
2026-02-04 11:29:40 +0100

    tp 4

--- monscript.js
+++ monscript.js
@@ -1,1 +1,7 @@
-alert("Ceci vient de mon fichier js")
+let tabJours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+let nomJour = tabJours[2];
+let numJour = 4;
+let nomMois = "Février";
+let isHiver = true;
+
+console.log(nomJour, numJour, nomMois, isHiver);
