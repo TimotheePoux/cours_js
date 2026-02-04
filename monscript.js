d13d8b0ffc8dbc3c3c9a97bbf866e878af56cf7b
Timothée Poux <timothee.poux@gmail.com>
2026-02-04 11:43:18 +0100

    TP 6

--- monscript.js
+++ monscript.js
@@ -8,7 +8,16 @@ let maDateV2 = {};
 maDateV2.jour = tabJours[2];
 maDateV2.mois = tabMois[1];
 
-let numJour = 4;
-let isHiver = true;
+let nbr1 = 3;
+let nbr2 = 7;
+let nbr3 = 8;
+let nbr4 = 21;
 
-console.log('V1: ',maDateV1.jour, maDateV1.mois, 'V2: ',maDateV2.jour, maDateV2.mois);
+console.log(nbr1 + nbr2, nbr4 - nbr3);
+
+nbr2 = nbr2*nbr1;
+console.log(nbr2);
+nbr2++;
+console.log(nbr2);
+
+console.log(nbr4/nbr1, nbr4%nbr1);
