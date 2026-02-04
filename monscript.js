78cd400ecec4b23ee7d33845e680161f2f5c2997
Timothée Poux <timothee.poux@gmail.com>
2026-02-04 11:37:54 +0100

    TP 5

--- monscript.js
+++ monscript.js
@@ -1,7 +1,14 @@
 let tabJours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-let nomJour = tabJours[2];
+let tabMois = ["Janvier, Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+let maDateV1 = {
+    "jour" : tabJours[2],
+    "mois" : tabMois[1]
+};
+let maDateV2 = {};
+maDateV2.jour = tabJours[2];
+maDateV2.mois = tabMois[1];
+
 let numJour = 4;
-let nomMois = "Février";
 let isHiver = true;
 
-console.log(nomJour, numJour, nomMois, isHiver);
+console.log('V1: ',maDateV1.jour, maDateV1.mois, 'V2: ',maDateV2.jour, maDateV2.mois);
