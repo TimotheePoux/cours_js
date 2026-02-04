0c462cad0319bc4d08922f351cacab98d5868759
Timothée Poux <timothee.poux@gmail.com>
2026-02-04 12:14:51 +0100

    TP9

--- monscript.js
+++ monscript.js
@@ -1,3 +1,4 @@
+/*
 let number = 5;
 let text = '5';
 let isRainingToday = false;
@@ -5,3 +6,19 @@ let isRainingToday = false;
 console.log(number == text);
 console.log(number === text);
 if (isRainingToday) console.log("Where is my Umbrella ?");
+*/
+
+let ageUser = prompt("T'as quel âge ?")
+
+if (ageUser < 0){
+    console.log("Je te crois pas");    
+}
+else if (ageUser < 18){
+    console.log("L’utilisateur a ", ageUser," an(s), il est mineur!");
+}
+else if (ageUser > 62){
+    console.log("L’utilisateur a ", ageUser ," an(s), il est majeur mais aussi retraité !!")
+}
+else{
+    console.log("L’utilisateur a ", ageUser," an(s), il est majeur!");
+}
