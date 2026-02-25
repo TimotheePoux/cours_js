37252745e8ca22d71413dc4b59c61f572f126186
Timothée Poux <timothee.poux@gmail.com>
2026-02-25 09:05:02 +0100

    hop

--- monscript.js
+++ monscript.js
@@ -8,17 +8,23 @@ console.log(number === text);
 if (isRainingToday) console.log("Where is my Umbrella ?");
 */
 
-let ageUser = prompt("T'as quel âge ?")
+let ageUser = prompt("T'as quel âge ?") //Demande à l'utilisateur quel âge il a
 
-if (ageUser < 0){
+if (ageUser < 0){ //Vérifie si l'âge de l'utilisateur est négatif
     console.log("Je te crois pas");    
 }
-else if (ageUser < 18){
+else if (ageUser == 18){
+    console.log("L’utilisateur a ", ageUser," an(s),il vient d’être majeur");
+}
+else if (ageUser == 25){
+    console.log("L’utilisateur a ", ageUser," an(s),il vient d’être majeur");
+}
+else if (ageUser < 18){ //Vérifie si l'âge de l'utilisateur est inférieur à 18
     console.log("L’utilisateur a ", ageUser," an(s), il est mineur!");
 }
-else if (ageUser > 62){
+else if (ageUser > 62){ //Vérifie si l'âge de l'utilisateur est supérieur à 62
     console.log("L’utilisateur a ", ageUser ," an(s), il est majeur mais aussi retraité !!")
 }
-else{
+else{ 
     console.log("L’utilisateur a ", ageUser," an(s), il est majeur!");
 }
