a478408cf94f9743ad4a21c00c64e6ef53e335fd
Timothée Poux <timothee.poux@gmail.com>
2026-02-25 10:09:51 +0100

    calcul

--- monscript.js
+++ monscript.js
@@ -1,27 +1,4 @@
-/*
-let toAdd = 0;
-let result = 0;
-while(toAdd<100){
-    toAdd++;
-    result+=toAdd;
+function calcul(annee){
+    return 2026-annee
 }
-console.log(result);
-*/
-
-let nb = 0;
-do{
-    nb = prompt("Saisir un nombre entre 1 et 100");
-}while(nb<1 || nb>100);
-
-let toAdd = 0;
-let result = 0;
-while(toAdd<nb){
-    toAdd++;
-    result+=toAdd;
-}   
-console.log(result);
-
-let tab = ["Axel", "Bako Ebeleh", "maaeeel", "(●'◡'●)", "C4tb0y4ev3r", "Cacaramouille", "Clemy", "firix le caillou", "isa", "Lauraaaaaa"];
-for(const val of tab){
-    console.log(val);
-}
+console.log(calcul(2006))
